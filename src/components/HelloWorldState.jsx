1ed8279e2dade4f838fab913ab6d53e1e5fefc31
Lucasfi <lukafigueroa64@gmail.com>
2025-06-12 23:24:33 -0300

    desabilitamos el boton una vez presionado para evitar repetir texto

--- src/components/HelloWorldState.jsx
+++ src/components/HelloWorldState.jsx
@@ -4,18 +4,20 @@ import { useState } from "react";
 const HelloWorldState = () => {
 
   const [mensaje, setMensajes] = useState('')
+  const [boton, setBotons] = useState(false)
 
   const texto = '(from changed state)!'
   const agregarMensaje = () => {
     console.log('Aqui debo agregar el mensaje')
     setMensajes(mensaje + texto)
     console.log(mensaje)
+    setBotons(true)
   }
 
   return (
     <div className="container my-5 text-white">
       <h1>Hello World {mensaje}</h1>
-      <Button variant="primary" className="mx-2" onClick={agregarMensaje}>Click me!</Button>
+      <Button variant="primary" className="mx-2" onClick={agregarMensaje} disabled={boton}>Click me!</Button>
     </div>
   );
 };
